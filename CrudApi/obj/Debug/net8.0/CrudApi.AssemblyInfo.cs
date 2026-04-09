c8581ab07d768974da5ea45c430553d07a7bdb05
Jhon Bohorquez <firsoftsi@gmail.com>
2026-04-08 19:49:07 -0500

    Actualización de librerias

--- CrudApi/obj/Debug/net8.0/CrudApi.AssemblyInfo.cs
+++ CrudApi/obj/Debug/net8.0/CrudApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc87756756bfa9ad18c4f0ed01b6b032125298c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f7d69c5cce95e2541634ee87802b5151916814")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
